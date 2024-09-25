40f22f58e88c4ebf6ce98548fe899483cbbb1753
Tafsirul Islam Shafin <107920202+0SHAFIN@users.noreply.github.com>
2024-09-25 23:30:41 +0600

    test commit

---
+++ view/problem_List.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Problem List</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<style>
+
+    body{
+        background-image: url('../files/designImg/plbg.png');
+        background-position: center;
+        background-size: cover;
+     
+    }
+    #mLogo{
+        margin-left: 10px;
+    }
+    #navSun{
+        color: #0B6477;
+        margin-right: 30px;
+        margin-bottom: 15px;
+    }
+    #nav-text{
+        color: #0B6477;
+    }
+
+    hr{
+        border: 1px solid #0B6477;
+    }
+
+    
+    
+</style>
+<body>
+    <nav class="navbar navbar-expand-sm ">
+        <img id="mLogo" src="../files/designImg/logo.png" alt="logo" style="width: 100px; height: 100%;">
+        <div class="navbar-nav mx-auto">
+            <ul class="navbar-nav ">
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text" href="#">Home</a></li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Catalog</a> </li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Contest </a></li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Problem Set </a></li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Ranking </a></li>
+            </ul>
+        </div>
+        <div class="d-flex bd-highlight mx-4">
+            <p class="mx-2 p-2 fw-bold" id="nav-text" >Login</p>
+            <div class="vr mt-2"style="border-left: 2px solid black; height: 26px;"></div>
+            <p class="mx-2 p-2 fw-bold" id="nav-text" ><a href="#" style="text-decoration:none;color: #2d5550;" id="signUp">Signup</a></p>
+        </div>
+        <div class="">
+            <i class="fa-solid fa-sun " id="navSun"></i>
+        </div>
+    </nav>
+   
+    <div class="row p-2">
+        <div class="col-4">
+        </div>
+        <div class="col-8">
+            <h2 class="fw-bold text-center" id="nav-text"> Problem Set</h2>
+            <div class="container">
+            <table class="table table-bordered">
+                <tr>
+                    <th>Id</th>
+                    <th>Problems</th>
+                    <th>Topics</th>
+                    <th>Rating</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>The infinnity Maze Challange</td>
+                    <td>Graph Theory</td>
+                    <td>2301</td>
+                </tr>
+             
+            </table>
+            </div>
+        </div>
+    </div>
+   
+</body>
+
+</html>
---
+++ view/problem_page.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<style>
+    /* body{
+        background-image: url("../files/designImg/plbg.jpeg");
+        background-size: cover;
+        background-repeat: no-repeat;
+     
+    } */
+    #mLogo{
+        margin-left: 10px;
+    }
+    #navSun{
+        color: #0B6477;
+        margin-right: 30px;
+        margin-bottom: 15px;
+    }
+    #nav-text{
+        color: #0B6477;
+    }
+
+    hr{
+        border: 1px solid #0B6477;
+    }
+
+</style>
+<body>
+<nav class="navbar navbar-expand-sm ">
+        <img id="mLogo" src="../files/designImg/logo.png" alt="logo" style="width: 100px; height: 100%;">
+        <div class="navbar-nav mx-auto">
+            <ul class="navbar-nav ">
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text" href="#">Home</a></li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Catalog</a> </li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Contest </a></li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Problem Set </a></li>
+                <li class="nav-item px-3 fw-bold"><a class="nav-link " id="nav-text"  href="#">Ranking </a></li>
+            </ul>
+        </div>
+        <div class="d-flex bd-highlight mx-4">
+            <p class="mx-2 p-2 fw-bold" id="nav-text" >Login</p>
+            <div class="vr mt-2"style="border-left: 2px solid black; height: 26px;"></div>
+            <p class="mx-2 p-2 fw-bold" id="nav-text" ><a href="#" style="text-decoration:none;color: #2d5550;" id="signUp">Signup</a></p>
+        </div>
+        <div class="">
+            <i class="fa-solid fa-sun " id="navSun"></i>
+        </div>
+</nav>
+<div class="container-fluid" style="background-color:#14919b;height:50px">
+    
+</div>
+
+<div class="row">
+    <div class="col">
+
+    <div class="container" style="background-color:white">
+      <h4 id="nav-text">Proble title</h4>
+      <p>
+        lrem ipsum dolor sit amet consectetur adipisicing elit
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae inventore earum, praesentium sunt debitis, itaque quis voluptatibus fugiat rem ad hic odit voluptate laborum consequatur cum, repellat aut! Aut, tenetur.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, maiores repellat at nulla excepturi laborum beatae. Illo quasi accusantium, in molestias asperiores minima laudantium vitae deleniti, quidem ipsum fuga doloribus!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit impedit autem, hic vero officiis et atque nostrum ducimus minima quod at maiores nobis expedita recusandae, laborum voluptate! Quidem, ducimus sint.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum molestias excepturi delectus laborum ab itaque, eum ut, deserunt accusamus assumenda et quasi quos, totam ea quod illum fugiat iusto expedita.
+    </p>
+    <h6>Input Format</h6>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eaque iusto, voluptatibus illum facere et, repellendus qui quidem, fugiat beatae aut magni eum ipsa sint itaque hic minima blanditiis. Nesciunt.
+    </p>
+    <h6>
+        Output Format
+    </h6>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eaque iusto, voluptatibus illum facere et, repellendus qui quidem, fugiat beatae aut magni eum ipsa sint itaque hic minima blanditiis. Nesciunt.
+   </p> 
+   <h6>Constraints</h6>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+
+    <div>
+        <table class="table table-bordered">
+            <tr>
+                <th>
+                    <div class="d-flex bd-highlight justify-content-between">
+                        <div>
+                            Input
+                        </div>
+                        <div>
+                            <button class="fw-bold" style="border:none;background-color:transparent">Copy</button>
+                        </div>
+                    </div>
+                </th>
+                <th>Output</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>4</td>
+            </tr>
+        </table>
+    </div>
+
+    <h6>Explanation:</h6>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eaque iusto, voluptatibus illum facere et, repellendus qui quidem, fugiat beatae aut magni eum ipsa sint itaque hic minima blanditiis. Nesciunt.
+    </p>
+
+    </div>
+
+
+
+    </div>
+    <div class="col mx-4">
+        <div class="container" style="background-color:#14919b;height: 100%;width: 100%;";>
+            <div class="container d-flex bd-highlight mt-4 justify-content-between p-2"style="background-color:#0b6477; width: 905px;" >
+                <div class="text-white px-3">
+                    Code
+                </div>
+                <div class="px-3">
+                    <select name="" id="" style="background-color:#0b6477;color:white; border-radius: 10px; border:1px solid white">
+                        <option value="">C</option>
+                        <option value="">C++</option>
+                    </select>
+                </div>
+            </div>
+            <div class="container" >
+                <textarea  name="codeArea" id="" cols="120" rows="18" class=" " style="background-color:#00455a;color:white;width:auto;width:auto"></textarea>
+            </div>
+            <div class="container d-flex bd-highlight justify-content-between p-2"style="background-color:#0b6477; width: 905px;" >
+                <div class="text-white px-3">
+                    Input Console
+                </div>
+                <div class="px-3">
+                    <input type="file" name="file" id="file" style="background-color:#0b6477;color:white; border-radius: 10px; border:1px solid white">
+                </div>
+            </div>
+            <div class="container" >
+                <textarea  name="codeArea" id="" cols="120" rows="5" class=" " style="background-color:#80b1c0;color:white;width:auto;width:auto"></textarea>
+            </div>
+            <div class="d-flex bd-hightlight justify-content-center">
+                <button class="mx-2" style="background-color:transparent;color:white;border-radius: 4px; border:1px solid white ">Run</button>
+                <button class="mx-2" style="background-color:transparent;color:white;border-radius: 4px; border:1px solid white ">Submit</button>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+</div>
+
+</body>
+</html>
